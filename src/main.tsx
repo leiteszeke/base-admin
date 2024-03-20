feac6982cfdfd32541196114f04e6522fe8aa902
Ezequiel Leites <ezequiel@leites.dev>
2024-03-20 19:51:45 +0100

    feat: improve css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./assets/react-search-select.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
